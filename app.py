df611ac65c41027424705aeaf7379b11b5bd402e
Edward Glen <eglen.git@gmail.com>
2021-04-27 07:22:50 -0700

    Disable flask logging.
    Don't need every request logged.

--- app.py
+++ app.py
@@ -2,7 +2,23 @@ from flask import Flask, render_template, request
 from flask.logging import default_handler
 from time import sleep
 import pigpio
-import logging
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 # The servos are connected on these GPIO pins (BCM numbering)
 HORIZ_SERVO_PORT = 13
