c4e92705dd80909892cc2ba8c0d643d17c45deb0
Edward Glen <eglen.git@gmail.com>
2021-05-08 06:13:45 +0100

    Migrate to sockets from spamming HTTP requests
    Doesn't help overly much with the latency issue over wifi but does
    ensure the changes come in order.
    
    Could have also worked around this by sending sequence number with the
    REST requests and discarding requests that were out of order.
    
    I think the next step is to tweak the AP settings to see if we can get
    some better performance there otherwise the video feed is just too heavy
    and we'll need to do some compression there.
    
    Oh yeah, also migrated to serving mjpeg stream externally rather than
    through flask. Don't think that made much of a difference really.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, Response
 from flask.logging import default_handler
-from camera_pi import Camera
+from flask_socketio import SocketIO, emit
 import threading
 from time import sleep
 from logging.config import dictConfig
@@ -48,8 +48,9 @@ HORIZ_CONVERSION = (HORIZ_SERVO_MAX - HORIZ_SERVO_CENTER)/MAX_COMMAND
 VERT_CONVERSION = (VERT_SERVO_MAX - VERT_SERVO_CENTER)/MAX_COMMAND
 
 # Create Flask App
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='', static_folder='lib')
+app.config['SECRET_KEY'] = 'prasutagus!'
+socketio = SocketIO(app)
 
 # GPIO Info
 gpio = pigpio.pi()
@@ -69,48 +70,29 @@ def clamp(num, minimum, maximum):
 def serveRoot():
     return render_template('main.html')
 
-# Expose an endpoint for sending the servo coordinates
-# from the JS to the Flask Backend
-@app.route("/moveservos", methods=["POST"])
-def moveServos():
-    #Data LED ON
+@socketio.on('rudder')
+def handle_rudder(rudderPos):
     gpio.write(ACTIVITY_PORT, 1)
-
-    # Get the values from the request
-    horizontal = HORIZ_CONVERSION * int(request.form["horizontal"])
-    vertical = VERT_CONVERSION * int(request.form["vertical"])
-
-    print("H: " + str(horizontal) + "\t" + str(HORIZ_SERVO_CENTER - horizontal) + "\t" + str(clamp(HORIZ_SERVO_CENTER - horizontal, HORIZ_SERVO_MIN, HORIZ_SERVO_MAX)) +
-          "\t V: " + str(vertical) + "\t" + str(VERT_SERVO_CENTER - vertical) + "\t" + str(clamp(VERT_SERVO_CENTER - vertical, VERT_SERVO_MIN, VERT_SERVO_MAX)))
-
-    # Move the Servos
+    print('rudder' + str(rudderPos))
+    horizontal = HORIZ_CONVERSION * int(rudderPos)
     setServoDuty(HORIZ_SERVO_PORT, clamp(HORIZ_SERVO_CENTER - horizontal, HORIZ_SERVO_MIN, HORIZ_SERVO_MAX))
-    setServoDuty(VERT_SERVO_PORT, clamp(VERT_SERVO_CENTER - vertical, VERT_SERVO_MIN, VERT_SERVO_MAX))
-
-    # Don't sleep in the handling thread, leave that to the client
-    #sleep(0.05)
-
-    # We've got enough power and high enough quality servos to prioritize responsiveness
-    #gpio.set_servo_pulsewidth(HORIZ_SERVO_PORT, 0)
-    #gpio.set_servo_pulsewidth(VERT_SERVO_PORT, 0)
-
-    #Data LED OFF
     gpio.write(ACTIVITY_PORT, 0)
 
-    # Return empty request (Should return a 200 OK with an empty body)
-    return ""
+@socketio.on('throttle')
+def test_message(throttlePos):
+    gpio.write(ACTIVITY_PORT, 1)
+    print('throttle' + str(throttlePos))
+    vertical = VERT_CONVERSION * int(throttlePos)
+    setServoDuty(VERT_SERVO_PORT, clamp(VERT_SERVO_CENTER - vertical, VERT_SERVO_MIN, VERT_SERVO_MAX))
+    gpio.write(ACTIVITY_PORT, 0)
 
-#https://blog.miguelgrinberg.com/post/video-streaming-with-flask
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+@socketio.on('connect')
+def connect():
+    print('Client connected')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@socketio.on('disconnect')
+def disconnect():
+    print('Client disconnected')
 
 def app_liveness_led():
     #App LED blink pattern
@@ -128,10 +110,11 @@ def app_liveness_led():
 # Run the app on the local development server
 # Accept any IP address
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", ssl_context='adhoc')
+    print("Starting")
     liveness = threading.Thread(target=app_liveness_led, args=())
     liveness.daemon = True
     liveness.start()
-    app.run(host="0.0.0.0")
+    print("Starting2")
     app.logger.removeHandler(default_handler)
+    socketio.run(app, host="0.0.0.0")
 
